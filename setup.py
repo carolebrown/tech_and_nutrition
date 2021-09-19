eb23af05c81f51e67ec6e6c0a8208931c3ddd876
Carole Ly <carole.ly.brown@gmail.com>
2021-09-18 18:53:41 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='How access to technology imp prommotes family nutrition',
+    author='Carole Brown',
+    license='',
+)
